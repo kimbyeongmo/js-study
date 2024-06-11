7c64889a58f82f3d9a630c1248bc1dc5576a093d
kimbyeongmo <kbmomo9999@naver.com>
2024-06-11 11:19:30 +0900

    # ProductItem 클래스 생성

---
+++ 4. OOP/js/components/ProductItem.js
@@ -0,0 +1,29 @@
+class ProductItem {
+
+  constructor(prod) {
+    this.prod = prod;
+  }
+
+  render() {
+
+    const { title, price, image, desc } = this.prod;
+
+    const $prodLi = document.createElement('li');
+    $prodLi.classList.add('product-item');
+    $prodLi.innerHTML = `
+      <div>
+        <img src="${image}" alt="${title}">
+        <div class="product-item__content">
+          <h2>${title}</h2>
+          <h3>${price}원</h3>
+          <p>${desc}</p>
+          <button>담기</button>
+        </div>
+      </div>
+    `;
+
+    return $prodLi;
+  }
+}
+
+export default ProductItem;
